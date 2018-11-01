9d0ede9475ddd6f6d4a4d342e83ccc7a2074104b
Michael Chekin <mchekin@gmail.com>
2018-11-01 19:29:33 +0100

    adding a routable Job page

--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Job
 
 
@@ -6,3 +6,8 @@ from .models import Job
 def home(request):
     jobs = Job.objects.all()
     return render(request, 'jobs/home.html', {'jobs': jobs})
+
+
+def detail(request, job_id):
+    job = get_object_or_404(Job, pk=job_id)
+    return render(request, 'jobs/detail.html', {'job': job})
